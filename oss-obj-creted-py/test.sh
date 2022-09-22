39789b11d5ee618ad82cbaeb053095be7670bd0a
fatih-keles <fatihkeles@gmail.com>
2022-09-23 00:07:05 +0200

    vision service cli tests

--- oss-obj-creted-py/test.sh
+++ oss-obj-creted-py/test.sh
@@ -1,3 +1,12 @@
+## Create a Dynamic Group DGFNforFatihKeles
+#ALL {resource.type = 'fnfunc', resource.compartment.id = 'ocid1.compartment.oc1..aaaaaaaapbatjdpgcbfpvwxmfkav5ijagbf7aepp5ln7xxlpl5ba347xukja'}
+
+## Add Policy 
+#Allow dynamic-group DGFNforFatihKeles to manage objects in compartment FatihKeles
+#Allow dynamic-group DGFNforFatihKeles> to buckets objects in compartment FatihKeles
+#Allow service objectstorage-uk-london-1 to manage object-family in tenancy
+
+
 ## deploy application 
 fn deploy --app document-processing-application
 
@@ -10,3 +19,5 @@ oci os object delete --force --bucket-name $_bucket_name --object-name $_object_
 oci os object put --bucket-name $_bucket_name --file $_file_name --name $_object_name
 
 ## oci os object copy --bucket-name $bucket_name --destination-bucket $destination_bucket --source-object-name $source_object_name
+## oci ai-vision analyze-document --document file://analyze-document/document.json --features file://analyze-document/features.json
+## cat output2.json | jq '.data.pages[].lines[].text'
